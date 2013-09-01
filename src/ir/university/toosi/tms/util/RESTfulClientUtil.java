57f31b45137f53940d120c45374eeb295f0f4f47
Farzad <farzad.sedaghatbin@gmail.com>
2013-09-01 14:16:22 +0430

    farzad

--- src/ir/university/toosi/tms/util/RESTfulClientUtil.java
+++ src/ir/university/toosi/tms/util/RESTfulClientUtil.java
@@ -2,21 +2,15 @@ package ir.university.toosi.tms.util;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import ir.university.toosi.tms.model.entity.User;
 import ir.university.toosi.tms.model.entity.WorkGroup;
+import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @author : Hamed Hatami ,  Farzad Sedaghatbin, Atefeh Ahmadi, Mostafa Rastgar
@@ -25,7 +19,7 @@ import java.util.Map;
 
 public class RESTfulClientUtil {
 
-    public InputStream restFullService(String url, String serviceName, String jsonString) {
+    public String restFullService(String url, String serviceName, String jsonString) {
         try {
             HttpClient client = new DefaultHttpClient();
             HttpPost postRequest = new HttpPost(url + serviceName);
@@ -37,13 +31,15 @@ public class RESTfulClientUtil {
                         + response.getStatusLine().getStatusCode());
             }
 
-            return response.getEntity().getContent();
+            return IOUtils.toString(response.getEntity().getContent(), "UTF-8");
 
         } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
-    } public InputStream restFullServiceString(String url, String serviceName, String jsonString) {
+    }
+
+    public String restFullServiceString(String url, String serviceName, String jsonString) {
         try {
             HttpClient client = new DefaultHttpClient();
             HttpPost postRequest = new HttpPost(url + serviceName);
@@ -55,7 +51,7 @@ public class RESTfulClientUtil {
                         + response.getStatusLine().getStatusCode());
             }
 
-            return response.getEntity().getContent();
+            return IOUtils.toString(response.getEntity().getContent(), "UTF-8");
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -64,17 +60,17 @@ public class RESTfulClientUtil {
     }
 
 
-    public InputStream restFullService(String url, String serviceName) {
+    public String restFullService(String url, String serviceName) {
         try {
             HttpClient client = new DefaultHttpClient();
             HttpPost postRequest = new HttpPost(url + serviceName);
-            postRequest.setHeader("Content-type", "application/json");
+            postRequest.setHeader("Content-type", "application/json; charset=UTF-8");
             HttpResponse response = client.execute(postRequest);
             if (response.getStatusLine().getStatusCode() != 200) {
                 throw new RuntimeException("Failed : HTTP error code : "
                         + response.getStatusLine().getStatusCode());
             }
-            return response.getEntity().getContent();
+            return IOUtils.toString(response.getEntity().getContent(), "UTF-8");
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -86,14 +82,15 @@ public class RESTfulClientUtil {
         try {
             HttpClient client = new DefaultHttpClient();
             HttpPost postRequest = new HttpPost(url + serviceName);
-            postRequest.setHeader("Content-type", "application/json");
+            postRequest.setHeader("Content-type", "application/json; charset=UTF-8");
             HttpResponse response = client.execute(postRequest);
             if (response.getStatusLine().getStatusCode() != 200) {
                 throw new RuntimeException("Failed : HTTP error code : "
                         + response.getStatusLine().getStatusCode());
             }
 
-            List<WorkGroup> workGroupList = new ObjectMapper().readValue(response.getEntity().getContent(),  new TypeReference<List<WorkGroup>>(){} );
+            List<WorkGroup> workGroupList = new ObjectMapper().readValue(response.getEntity().getContent(), new TypeReference<List<WorkGroup>>() {
+            });
             client.getConnectionManager().shutdown();
             return workGroupList;
 
