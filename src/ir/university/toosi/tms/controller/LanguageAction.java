8d2265594c7883c4c5739df44182857c45255f65
javaee5 <javaee5@yahoo.com>
2013-09-01 17:27:16 +0430

    TMS

---
+++ src/ir/university/toosi/tms/controller/LanguageAction.java
@@ -0,0 +1,67 @@
+package ir.university.toosi.tms.controller;
+
+import com.google.common.io.Files;
+import ir.university.toosi.tms.util.LangUtils;
+
+import java.io.*;
+import java.text.MessageFormat;
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.Properties;
+import java.util.ResourceBundle;
+
+public class LanguageAction {
+
+    private static final String BUNDLE_NAME = "ir.university.toosi.tms.resources.Messages";
+    public static Locale locale = new Locale("fa");
+    private static Properties newLanguage= null;
+    private static boolean hasLanguage= false;
+
+
+    private static ResourceBundle getBundle() {
+        return ResourceBundle.getBundle(BUNDLE_NAME, getLocale());
+    }
+
+    public static String getBundleMessage(String bundleKey, String... arguments) {
+        if (bundleKey == null)
+            return "?";
+        if(hasLanguage){
+            return  getFromProperty(bundleKey);
+        }
+        try {
+            return MessageFormat.format(getBundle().getString(bundleKey), arguments);
+        } catch (MissingResourceException mre) {
+            return bundleKey;
+        }
+    }
+
+    public static void initProperty(byte[] content) throws IOException {
+        File f= new File(System.getenv("java.io.tmpdir"));
+        Files.write(content,f);
+        InputStream inputStream= new FileInputStream(f);
+        newLanguage.load(inputStream);
+    }
+
+    private static String getFromProperty(String bundleKey) {
+        return newLanguage.getProperty(bundleKey);
+    }
+
+    public static Locale getLocale() {
+        return LangUtils.refine(locale);
+    }
+
+    public static void changeLocale(String language) {
+        String currentLanguage = locale.getLanguage();
+
+        if (currentLanguage == null) {
+            locale = LangUtils.LOCALE_FARSI;
+        }
+
+        if (currentLanguage.equalsIgnoreCase("fa")) {
+            locale = new Locale(language);
+        } else if (language.equalsIgnoreCase("other")) {
+            hasLanguage= true;
+        }
+    }
+
+}
